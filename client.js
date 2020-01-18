d4585cd8b57e9d15abf8c5b847c293dac09e9539
admin <admin@pierres-macbook-pro-2.home>
2020-01-18 19:07:33 +0100

    first commit

---
+++ client.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const request = require('request-promise')
+const btoa = require('btoa')
+
+
+const { ISSUER, CLIENT_ID, CLIENT_SECRET, SCOPE } = process.env
+
+const [,, uri, method, body] = process.argv
+console.log(ISSUER,CLIENT_ID,CLIENT_SECRET,SCOPE)
+if (!uri) {
+  console.log('Usage: node client {url} [{method}] [{jsonData}]')
+  process.exit(1)
+}
+
+const sendAPIRequest = async () => {
+ 
+  const token = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)
+  try {
+    const auth = await request({
+      uri: `${ISSUER}/v1/token`,
+      json: true,
+      method: 'POST',
+      headers: {
+        authorization: `Basic ${token}`
+      },
+      form: {
+        grant_type: 'client_credentials',
+        scope: SCOPE
+      }
+    })
+    
+    const response = await request({
+      uri,
+      method,
+      body,
+      headers: {
+        authorization: `Bearer ${auth.access_token}`
+      }
+    })
+
+    console.log(auth)
+  } catch (error) {
+
+    console.log(`Error: ${error.message}`)
+  }
+}
+
+sendAPIRequest()
---
+++ database.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const  finale = require('finale-rest')
+
+const database = new Sequelize({
+  dialect: 'sqlite',
+  storage: './test.sqlite',
+
+})
+
+const Part = database.define('parts', {
+  partNumber: Sequelize.STRING,
+  modelNumber: Sequelize.STRING,
+  name: Sequelize.STRING,
+  description: Sequelize.TEXT
+})
+
+const initializeDatabase = async (app) => {
+  finale.initialize({ app, sequelize: database })
+
+  finale.resource({
+    model: Part,
+    endpoints: ['/parts', '/parts/:id']
+  })
+
+  await database.sync()
+}
+
+module.exports = initializeDatabase
